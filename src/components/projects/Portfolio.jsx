f60c7ce26078d5ed5fd9ec5c378181b068bc0dd8
pramit_sr <mepramitsarkarr@gmail.com>
2024-11-19 02:16:29 +0530

    chnages in portfolio.jsx

--- src/components/projects/Portfolio.jsx
+++ src/components/projects/Portfolio.jsx
@@ -5,8 +5,25 @@ import "./projects.css";
 const Portfolio = () => {
   return (
     <section className="portfolio section" id="portfolio">
-        <h2 className="section__title">My Projects</h2>
-        <span className="section__subtitle">Check out my recent Projects</span>
+        <div className="section__title">
+        <div className="flex justify-center items-center w-full">
+          <h2 
+            className="inline-block bg-black text-white px-6 py-3 rounded-xl text-4xl font-bold m-4"
+            style={{ display: "inline-block", backgroundColor: "#000", color: "#fff", borderRadius: "8px" , paddingLeft:"10px" , paddingRight:"10px" }}
+          >
+            My Projects
+          </h2>
+        </div>
+        </div>
+        <span className="section__subtitle" >
+        <h2 
+          className="text-5xl sm:text-7xl lg:text-8xl font-bold tracking-tight text-center mx-auto !text-7xl"
+          style={{ fontSize: "4rem", lineHeight: "1.2" }}
+        >
+          Check out my latest work
+        </h2>
+
+        </span>
         <Projects />
     </section>
   );
